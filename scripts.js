b79bd433854351f693678b4e0d8dcab46d4ed6ff
SpencerTong <slt140@scarletmail.rutgers.edu>
2023-05-03 18:55:23 -0400

    typos and desc modifications

--- scripts.js
+++ scripts.js
@@ -6,9 +6,8 @@ window.addEventListener("scroll", e => {
   let scrollPos = window.scrollY;
   if (scrollPos > navPos) {
     navbar.classList.add('sticky');
-    header.classList.add('navbarOffsetMargin');
   } else {
     navbar.classList.remove('sticky');
-    header.classList.remove('navbarOffsetMargin');
   }
 });
+
